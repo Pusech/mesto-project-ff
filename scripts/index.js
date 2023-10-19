738deb0e310bd25f49926a33ca6f7033b5058690
Pusech <iinuzukakiba@gmail.com>
2023-10-19 19:51:28 +0200

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,23 @@
 // @todo: Темплейт карточки
-const placeTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list");
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-const createCard = function () {
-  for (let i = 0; i < initialCards.length; i++) {
-    const placeElement = placeTemplate
-      .querySelector(".places__item")
-      .cloneNode(true);
-    placeElement.querySelector(".card__image").src = initialCards[i].link;
-    placeElement.querySelector(".card__title").textContent =
-      initialCards[i].name;
-    placesList.append(placeElement);
-  }
-  const deleteBtn = document.querySelectorAll(".card__delete-button");
-  deleteBtn.forEach((btn) =>
-    btn.addEventListener("click", function () {
-      deleteCard(btn);
-    })
-  );
+const createCard = (cardData, deleteCallback) => {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function () {
+      deleteCallback(cardElement);
+    });
+  return cardElement;
 };
 
 // @todo: Функция удаления карточки
@@ -30,4 +27,8 @@ function deleteCard(btn) {
 }
 
 // @todo: Вывести карточки на страницу
-createCard();
+const renderInitialCards = (function () {
+  initialCards.forEach((card) => {
+    cardList.append(createCard(card, deleteCard));
+  });
+})();
